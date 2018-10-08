65ec215f8fc84b83c7d46ffe6072dc40331cd0eb
Rose <rose1264@gmail.com>
2018-10-08 16:10:34 -0400

    refactor file structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
